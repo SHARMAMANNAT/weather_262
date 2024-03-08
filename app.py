6c9437065a343914cdd7d6c3687e60f8dd8e2975
unknown <sharmamannat103n@gmail.com>
2024-03-08 14:54:06 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
+#import flask library
+from flask import Flask,render_template,request
+import requests
+#initialize flask
+app=Flask(__name__)
+#route your webpage
 @app.route("/")
 def visitors():
 
@@ -18,21 +20,27 @@ def visitors():
     counter_write_file.write(str(visitors_count))
     counter_write_file.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
-
-@app.route('/', methods=['POST'])
-def covid_stats():
+# Render HTML with count variable
+    return render_template("index.html",count=visitors_count)
+#route your webpage
+@app.route('/',methods=['POST'])
+def weather_stats():
     # Load current count
     counter_read_file = open("count.txt", "r")
     visitors_count = int(counter_read_file.read())
     counter_read_file.close()
 
-    text = request.form['text']
+    #complete the code
+    latitude=request.form['latitude']
+    longitude=request.form['longitude']
+    
+    api_url='https://weather-l6tl.onrender.com/api/getCurrentWeather/'+latitude+'/'+longitude
+    
+    response=requests.get(api_url)
+    weather_data=response.json()
+    print(weather_data)
+    return render_template("index.html",weather=weather_data,count=visitors_count)
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
-
-if __name__ == "__main__":
+#add code for executing flask
+if __name__=="__main__":
     app.run()
